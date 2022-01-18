5b05b24583e7429a1ddf18e852cd93b3a21ea809
avishahi <87566871+avishahi@users.noreply.github.com>
2022-01-18 16:10:27 +0530

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,17 @@
-IR.onPressEvent(RemoteButton.Up, function () {
-    basic.showString("Hello!")
+STEMROBO.setup()
+makerbit.connectIrReceiver(DigitalPin.P16, IrProtocol.Keyestudio)
+basic.forever(function () {
+    if ("0x00FF601F" == makerbit.irDatagram()) {
+        STEMROBO.moveIt(MOVE.forward)
+    } else if ("0x00FF205F" == makerbit.irDatagram()) {
+        STEMROBO.moveIt(MOVE.backward)
+    } else if ("0x00FF304F" == makerbit.irDatagram()) {
+        STEMROBO.moveIt(MOVE.left)
+    } else if ("0x00FF502F" == makerbit.irDatagram()) {
+        STEMROBO.moveIt(MOVE.right)
+    } else if ("0x00FF245B" == makerbit.irDatagram()) {
+        STEMROBO.moveIt(MOVE.stop)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
 })
-IR.init(Pins.P16)
